535cb49782dd90e22219f6d9f8c1a6bec86bcd4a
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-11-23 12:45:57 -0500

    Initial commit

---
+++ Baubit.Tasks/TaskExtensions.cs
@@ -0,0 +1,52 @@
+﻿using FluentResults;
+
+namespace Baubit.Tasks
+{
+    public static class TaskExtensions
+    {
+        public static Result Wait(this Task task, bool ignoreTaskCancellationException = false)
+        {
+            try
+            {
+                task.Wait();
+                return Result.Ok();
+            }
+            catch (AggregateException aExp)
+            {
+                if (aExp.InnerException is TaskCanceledException)
+                {
+                    return ignoreTaskCancellationException ? Result.Ok() : Result.Fail(new ExceptionalError(aExp.InnerException));
+                }
+                else
+                {
+                    return Result.Fail(new ExceptionalError(aExp));
+                }
+            }
+        }
+        public static async Task<Result> WaitAsync(this Task task, bool ignoreTaskCancellationException = false)
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+                return Result.Ok();
+            }
+            catch (AggregateException aExp)
+            {
+                if (aExp.InnerException is TaskCanceledException)
+                {
+                    return ignoreTaskCancellationException ? Result.Ok() : Result.Fail(new ExceptionalError(aExp.InnerException));
+                }
+                else
+                {
+                    return Result.Fail(new ExceptionalError(aExp));
+                }
+            }
+        }
+
+        public static Result RegisterCancellationToken<T>(this TaskCompletionSource<T> taskCompletionSource, CancellationToken cancellationToken)
+        {
+            return Result.Try(() => cancellationToken.Register(() => taskCompletionSource.TrySetCanceled(cancellationToken), useSynchronizationContext: false))
+                         .Bind(registration => Result.Try(() => { taskCompletionSource.Task.ContinueWith(_ => registration.Dispose(), TaskScheduler.Default); }));
+        }
+    }
+}
---
+++ Baubit.Tasks/TimedCancellationTokenSource.cs
@@ -0,0 +1,53 @@
+﻿namespace Baubit.Tasks
+{
+    public class TimedCancellationTokenSource : CancellationTokenSource
+    {
+        public TimeSpan Timeout { get; init; }
+
+        public new bool IsCancellationRequested
+        {
+            get
+            {
+                if (!cancellationTriggered)
+                {
+                    CancelAfter(Timeout);
+                    cancellationTriggered = true;
+                }
+                return base.IsCancellationRequested;
+            }
+        }
+
+        public new CancellationToken Token
+        {
+            get
+            {
+                if (timerStartsAtTokenAccess && !cancellationTriggered)
+                {
+                    CancelAfter(Timeout);
+                    cancellationTriggered = true;
+                }
+                return base.Token;
+            }
+        }
+
+        private bool timerStartsAtTokenAccess;
+        private bool cancellationTriggered;
+        public TimedCancellationTokenSource(uint millisecondTimeout, bool timerStartsAtTokenAccess = true) : this(new TimeSpan(0, 0, 0, 0, (int)millisecondTimeout), timerStartsAtTokenAccess) { }
+        public TimedCancellationTokenSource(TimeSpan? timeOut, bool timerStartsAtTokenAccess = true)
+        {
+            Timeout = timeOut ?? System.Threading.Timeout.InfiniteTimeSpan;
+            this.timerStartsAtTokenAccess = timerStartsAtTokenAccess;
+
+        }
+
+        public new bool TryReset()
+        {
+            var retVal = base.TryReset();
+            if (retVal)
+            {
+                cancellationTriggered = false;
+            }
+            return retVal;
+        }
+    }
+}
